50746d3b5b1c6f60558b47f53a18629889da1860
Neil Ang <neilang@gmail.com>
2014-04-25 12:36:54 +1000

    Added feedback endpoints

--- api/api.rb
+++ api/api.rb
@@ -7,6 +7,20 @@ module LineStatus
       { database: ActiveRecord::Base.connection.active? }
     end
 
+    resource :feedback do
+
+      desc "Retrieve user feedback for a line/direction ID"
+      get ':linedir' do
+        { status: 'A-Okay' }
+      end
+
+      desc "Create/update user feedback for a line/direction ID"
+      post ':linedir' do
+        { status: 'Updated' }
+      end
+
+    end
+
     # Make Padrino happy
     class << self
       def app_name; ''; end
--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -9,8 +9,13 @@ describe LineStatus::API, request: true do
     results['database'].should be_true
   end
 
-  it "can look up feedback for a line"
+  it "can look up feedback for a line" do
+    get "/api/feedback/1234"
+    last_response.should be_successful
+  end
 
-  it "can post feedback for a line"
-
+  it "can post feedback for a line" do
+    post "/api/feedback/1234"
+    last_response.should be_successful
+  end
 end
